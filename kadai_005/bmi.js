e6231625c75cdf4588c39f14e672e3e54157e784
mm-miyuki <tomo.miyu.0704@gmail.com>
2024-06-28 01:00:39 +0900

    bmi.js

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -4,7 +4,8 @@ let weight = 68;
 //身長を代入する
 let height = 1.7;
 
-//bmiの計算式
-let bmi(weight/(height*height));
-//bmiの計算結果の出力
+//BMIの計算式
+let bmi = weight/(height*height);
+
+//BMIの計算結果の出力
 console.log(bmi);
